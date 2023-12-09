ce3bb1f6b770b1001d68112a5722548ecdba0c60
Oswaldo <oswaldoaraelg@gmail.com>
2023-12-09 16:55:05 -0600

    Se corrige la versión del odbc

--- database.py
+++ database.py
@@ -2,5 +2,5 @@ from sqlalchemy import create_engine
 
 
 # Configura la conexión a la base de datos con SQLAlchemy
-DATABASE_URL = "mssql+pyodbc://sa:Trotamundosserver@trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com/trotamundosdb?driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.2.so.0.1'"
+DATABASE_URL = "mssql+pyodbc://sa:Trotamundosserver@trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com/trotamundosdb?driver=ODBC+Driver+17+for+SQL+Server"
 engine = create_engine(DATABASE_URL, echo=True)
