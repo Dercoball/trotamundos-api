e7d1d6824051197bb55529b0c320c704e9eb835e
Oswaldo <oswaldoaraelg@gmail.com>
2023-12-10 16:16:54 -0600

    Version de Driver SQL

--- database.py
+++ database.py
@@ -2,5 +2,7 @@ from sqlalchemy import create_engine
 
 
 # Configura la conexión a la base de datos con SQLAlchemy
-DATABASE_URL = "mssql+pyodbc://sa:Trotamundosserver@trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com/trotamundosdb?driver=ODBC+Driver+17+for+SQL+Server"
-engine = create_engine(DATABASE_URL, echo=True)
+#DATABASE_URL = "mssql+pyodbc://sa:Trotamundosserver@trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com/trotamundosdb?driver=ODBC+Driver+17+for+SQL+Server"
+#engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine('mssql+pyodbc://sa:Trotamundosserver@trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com/trotamundosdb?driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.5.1', echo=True)
+engine.execute('select 1')
