2f7fda1ade202745e78440e27fc4e10dafd6dafc
AndresDev <you@example.com>
2023-12-11 17:51:22 -0600

    odbc

--- database.py
+++ database.py
@@ -1,6 +1,18 @@
 from sqlalchemy import create_engine
 
+# SERVER = 'trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com'
+# DATABASE = 'trotamundosdb'
+# DRIVER = 'SQL Server Native Client 11.0'
+# USERNAME = 'sa'
+# PASSWORD = 'Trotamundosserver'
+# DATABASE_CONNECTION = f'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}'
 
-# Configura la conexión a la base de datos con SQLAlchemy
-DATABASE_URL = "mssql+pyodbc://sa:Trotamundosserver@trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com/trotamundosdb?driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.5.1'"
+# engine = create_engine(DATABASE_CONNECTION)
+# connection = engine.connect()
+# # Configura la conexión a la base de datos con SQLAlchemy
+# # engine = create_engine("mssql+pyodbc://sa:Trotamundosserver@trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com/trotamundosdb?+Driver+17+for+SQL+Server")
+# # DATABASE_URL = "mssql+pyodbc://sa:Trotamundosserver@trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com/trotamundosdb?driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.5.1'"
+# # engine = create_engine(DATABASE_URL, echo=True)
+
+DATABASE_URL = "mssql+pyodbc://sa:Trotamundosserver@trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com/trotamundosdb?driver=ODBC+Driver+17+for+SQL+Server"
 engine = create_engine(DATABASE_URL, echo=True)
