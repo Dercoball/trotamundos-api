6cad366ba9cb478538c72417a8d07ccbc7320c43
Oswaldo <oswaldoaraelg@gmail.com>
2024-07-30 16:25:24 -0600

    Se cambian variables del model

--- modelos/modelos.py
+++ modelos/modelos.py
@@ -94,6 +94,7 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class TokenData(BaseModel):
     username: Optional[str] = None
 class DatosLogin(BaseModel):
@@ -179,224 +180,226 @@ class Empleado(BaseModel):
     Estatus: int
 
 class Checklist(BaseModel):
-    Lectura_codigos: Optional[int] = None
-    Lectura_codigos_observacion: Optional[str] = None
-    Lectura_codigos_foto: Optional[str] = None
+    lectura_codigos: Optional[int] = None
+    lectura_codigos_observacion: Optional[str] = None
+    lectura_codigos_foto: Optional[str] = None
 
-    Servofreno: Optional[int] = None
-    Servofreno_observacion: Optional[str] = None
-    Servofreno_foto: Optional[str] = None
+    servofreno: Optional[int] = None
+    servofreno_observacion: Optional[str] = None
+    servofreno_foto: Optional[str] = None
 
-    Pedal_freno: Optional[int] = None
-    Pedal_freno_observacion: Optional[str] = None
-    Pedal_freno_foto: Optional[str] = None
+    pedal_freno: Optional[int] = None
+    pedal_freno_observacion: Optional[str] = None
+    pedal_freno_foto: Optional[str] = None
 
-    Pedal_estacionamiento: Optional[int] = None
-    Pedal_estacionamiento_observacion: Optional[str] = None
-    Pedal_estacionamiento_foto: Optional[str] = None
+    pedal_estacionamiento: Optional[int] = None
+    pedal_estacionamiento_observacion: Optional[str] = None
+    pedal_estacionamiento_foto: Optional[str] = None
 
-    Cinturon_seguridad: Optional[int] = None
-    Cinturon_seguridad_observacion: Optional[str] = None
-    Cinturon_seguridad_foto: Optional[str] = None
+    cinturon_seguridad: Optional[int] = None
+    cinturon_seguridad_observacion: Optional[str] = None
+    cinturon_seguridad_foto: Optional[str] = None
 
-    Cuadro_instrumentos: Optional[int] = None
-    Cuadro_instrumentos_observacion: Optional[str] = None
-    Cuadro_instrumentos_foto: Optional[str] = None
+    cuadro_instrumentos: Optional[int] = None
+    cuadro_instrumentos_observacion: Optional[str] = None
+    cuadro_instrumentos_foto: Optional[str] = None
 
-    Aire_acondicionado: Optional[int] = None
-    Aire_acondicionado_observacion: Optional[str] = None
-    Aire_acondicionado_foto: Optional[str] = None
+    aire_acondicionado: Optional[int] = None
+    aire_acondicionado_observacion: Optional[str] = None
+    aire_acondicionado_foto: Optional[str] = None
 
-    Bocina_claxon: Optional[int] = None
-    Bocina_claxon_observacion: Optional[str] = None
-    Bocina_claxon_foto: Optional[str] = None
+    bocina_claxon: Optional[int] = None
+    bocina_claxon_observacion: Optional[str] = None
+    bocina_claxon_foto: Optional[str] = None
 
-    Iluminacion_interior: Optional[int] = None
-    Iluminacion_interior_observacion: Optional[str] = None
-    Iluminacion_interior_foto: Optional[str] = None
+    iluminacion_interior: Optional[int] = None
+    iluminacion_interior_observacion: Optional[str] = None
+    iluminacion_interior_foto: Optional[str] = None
 
-    Iluminacion_externa: Optional[int] = None
-    Iluminacion_externa_observacion: Optional[str] = None
-    Iluminacion_externa_foto: Optional[str] = None
+    iluminacion_externa: Optional[int] = None
+    iluminacion_externa_observacion: Optional[str] = None
+    iluminacion_externa_foto: Optional[str] = None
 
-    Limpiaparabrisas: Optional[int] = None
-    Limpiaparabrisas_observacion: Optional[str] = None
-    Limpiaparabrisas_foto: Optional[str] = None
+    limpiaparabrisas: Optional[int] = None
+    limpiaparabrisas_observacion: Optional[str] = None
+    limpiaparabrisas_foto: Optional[str] = None
 
-    Limpia_medallon: Optional[int] = None
-    Limpia_medallon_observacion: Optional[str] = None
-    Limpia_medallon_foto: Optional[str] = None
+    limpia_medallon: Optional[int] = None
+    limpia_medallon_observacion: Optional[str] = None
+    limpia_medallon_foto: Optional[str] = None
 
-    Neumaticos_friccion: Optional[int] = None
-    Neumaticos_friccion_observacion: Optional[str] = None
-    Neumaticos_friccion_foto: Optional[str] = None
+    neumaticos_friccion: Optional[int] = None
+    neumaticos_friccion_observacion: Optional[str] = None
+    neumaticos_friccion_foto: Optional[str] = None
 
-    Otros_vehiculo_en_piso: Optional[int] = None
-    Otros_vehiculo_en_piso_observacion: Optional[str] = None
-    Otros_vehiculo_en_piso_foto: Optional[str] = None
+    otros_vehiculo_en_piso: Optional[int] = None
+    otros_vehiculo_en_piso_observacion: Optional[str] = None
+    otros_vehiculo_en_piso_foto: Optional[str] = None
 
-    Estado_fugas_aceite: Optional[int] = None
-    Estado_fugas_aceite_observacion: Optional[str] = None
-    Estado_fugas_aceite_foto: Optional[str] = None
+    estado_fugas_aceite: Optional[int] = None
+    estado_fugas_aceite_observacion: Optional[str] = None
+    estado_fugas_aceite_foto: Optional[str] = None
 
-    Estado_nivel_calidad_lubricante_transmision: Optional[int] = None
-    Estado_nivel_calidad_lubricante_transmision_observacion: Optional[str] = None
-    Estado_nivel_calidad_lubricante_transmision_foto: Optional[str] = None
+    estado_nivel_calidad_lubricante_transmision: Optional[int] = None
+    estado_nivel_calidad_lubricante_transmision_observacion: Optional[str] = None
+    estado_nivel_calidad_lubricante_transmision_foto: Optional[str] = None
 
-    Estado_nivel_calidad_lubricante_diferencial: Optional[int] = None
-    Estado_nivel_calidad_lubricante_diferencial_observacion: Optional[str] = None
-    Estado_nivel_calidad_lubricante_diferencial_foto: Optional[str] = None
+    estado_nivel_calidad_lubricante_diferencial: Optional[int] = None
+    estado_nivel_calidad_lubricante_diferencial_observacion: Optional[str] = None
+    estado_nivel_calidad_lubricante_diferencial_foto: Optional[str] = None
 
-    Cubrepolvos_flechas: Optional[int] = None
-    Cubrepolvos_flechas_observacion: Optional[str] = None
-    Cubrepolvos_flechas_foto: Optional[str] = None
+    cubrepolvos_flechas: Optional[int] = None
+    cubrepolvos_flechas_observacion: Optional[str] = None
+    cubrepolvos_flechas_foto: Optional[str] = None
 
-    Componentes_direccion: Optional[int] = None
-    Componentes_direccion_observacion: Optional[str] = None
-    Componentes_direccion_foto: Optional[str] = None
+    componentes_direccion: Optional[int] = None
+    componentes_direccion_observacion: Optional[str] = None
+    componentes_direccion_foto: Optional[str] = None
 
-    Componentes_suspesion: Optional[int] = None
-    Componentes_suspesion_observacion: Optional[str] = None
-    Componentes_suspesion_foto: Optional[str] = None
+    componentes_suspesion: Optional[int] = None
+    componentes_suspesion_observacion: Optional[str] = None
+    componentes_suspesion_foto: Optional[str] = None
 
-    Sistema_escape_completo: Optional[int] = None
-    Sistema_escape_completo_observacion: Optional[str] = None
-    Sistema_escape_completo_foto: Optional[str] = None
+    sistema_escape_completo: Optional[int] = None
+    sistema_escape_completo_observacion: Optional[str] = None
+    sistema_escape_completo_foto: Optional[str] = None
 
-    Sistema_alimentacion_combustible: Optional[int] = None
-    Sistema_alimentacion_combustible_observacion: Optional[str] = None
-    Sistema_alimentacion_combustible_foto: Optional[str] = None
+    sistema_alimentacion_combustible: Optional[int] = None
+    sistema_alimentacion_combustible_observacion: Optional[str] = None
+    sistema_alimentacion_combustible_foto: Optional[str] = None
 
-    Filtro_combustible: Optional[int] = None
-    Filtro_combustible_observacion: Optional[str] = None
-    Filtro_combustible_foto: Optional[str] = None
+    filtro_combustible: Optional[int] = None
+    filtro_combustible_observacion: Optional[str] = None
+    filtro_combustible_foto: Optional[str] = None
 
-    Control_fugas_direccion_hidraulica: Optional[int] = None
-    Control_fugas_direccion_hidraulica_observacion: Optional[str] = None
-    Control_fugas_direccion_hidraulica_foto: Optional[str] = None
+    control_fugas_direccion_hidraulica: Optional[int] = None
+    control_fugas_direccion_hidraulica_observacion: Optional[str] = None
+    control_fugas_direccion_hidraulica_foto: Optional[str] = None
 
-    Otros_altura_total: Optional[int] = None
-    Otros_altura_total_observacion: Optional[str] = None
-    Otros_altura_total_foto: Optional[str] = None
+    otros_altura_total: Optional[int] = None
+    otros_altura_total_observacion: Optional[str] = None
+    otros_altura_total_foto: Optional[str] = None
 
-    Rodamiento_mazas_rueda: Optional[int] = None
-    Rodamiento_mazas_rueda_observacion: Optional[str] = None
-    Rodamiento_mazas_rueda_foto: Optional[str] = None
+    rodamiento_mazas_rueda: Optional[int] = None
+    rodamiento_mazas_rueda_observacion: Optional[str] = None
+    rodamiento_mazas_rueda_foto: Optional[str] = None
 
-    Holgura_partes_suspension_rueda: Optional[int] = None
-    Holgura_partes_suspension_rueda_observacion: Optional[str] = None
-    Holgura_partes_suspension_rueda_foto: Optional[str] = None
+    holgura_partes_suspension_rueda: Optional[int] = None
+    holgura_partes_suspension_rueda_observacion: Optional[str] = None
+    holgura_partes_suspension_rueda_foto: Optional[str] = None
 
-    Control_neumaticos_desgaste_presion: Optional[int] = None
-    Control_neumaticos_desgaste_presion_observacion: Optional[str] = None
-    Control_neumaticos_desgaste_presion_foto: Optional[str] = None
+    control_neumaticos_desgaste_presion: Optional[int] = None
+    control_neumaticos_desgaste_presion_observacion: Optional[str] = None
+    control_neumaticos_desgaste_presion_foto: Optional[str] = None
 
-    Profundidad: Optional[int] = None
-    Profundidad_observacion: Optional[str] = None
-    Profundidad_foto: Optional[str] = None
+    profundidad: Optional[int] = None
+    profundidad_observacion: Optional[str] = None
+    profundidad_foto: Optional[str] = None
 
-    Presion: Optional[int] = None
-    Presion_observacion: Optional[str] = None
-    Presion_foto: Optional[str] = None
+    presion: Optional[int] = None
+    presion_observacion: Optional[str] = None
+    presion_foto: Optional[str] = None
 
-    Otros_altura_media: Optional[int] = None
-    Otros_altura_media_observacion: Optional[str] = None
-    Otros_altura_media_foto: Optional[str] = None
+    otros_altura_media: Optional[int] = None
+    otros_altura_media_observacion: Optional[str] = None
+    otros_altura_media_foto: Optional[str] = None
 
-    Nivel_calidad_aceite_motor: Optional[int] = None
-    Nivel_calidad_aceite_motor_observacion: Optional[str] = None
-    Nivel_calidad_aceite_motor_foto: Optional[str] = None
+    nivel_calidad_aceite_motor: Optional[int] = None
+    nivel_calidad_aceite_motor_observacion: Optional[str] = None
+    nivel_calidad_aceite_motor_foto: Optional[str] = None
 
-    Filtro_aire: Optional[int] = None
-    Filtro_aire_observacion: Optional[str] = None
-    Filtro_aire_foto: Optional[str] = None
+    filtro_aire: Optional[int] = None
+    filtro_aire_observacion: Optional[str] = None
+    filtro_aire_foto: Optional[str] = None
 
-    Filtro_polen: Optional[int] = None
-    Filtro_polen_observacion: Optional[str] = None
-    Filtro_polen_foto: Optional[str] = None
+    filtro_polen: Optional[int] = None
+    filtro_polen_observacion: Optional[str] = None
+    filtro_polen_foto: Optional[str] = None
 
-    Filtro_pcv: Optional[int] = None
-    Filtro_pcv_observacion: Optional[str] = None
-    Filtro_pcv_foto: Optional[str] = None
+    filtro_pcv: Optional[int] = None
+    filtro_pcv_observacion: Optional[str] = None
+    filtro_pcv_foto: Optional[str] = None
 
-    Valvula_pcv: Optional[int] = None
-    Valvula_pcv_observacion: Optional[str] = None
-    Valvula_pcv_foto: Optional[str] = None
+    valvula_pcv: Optional[int] = None
+    valvula_pcv_observacion: Optional[str] = None
+    valvula_pcv_foto: Optional[str] = None
 
-    Bujias_encendido: Optional[int] = None
-    Bujias_encendido_observacion: Optional[str] = None
-    Bujias_encendido_foto: Optional[str] = None
+    bujias_encendido: Optional[int] = None
+    bujias_encendido_observacion: Optional[str] = None
+    bujias_encendido_foto: Optional[str] = None
 
-    Cables_bujias_bobinas_ignicion: Optional[int] = None
-    Cables_bujias_bobinas_ignicion_observacion: Optional[str] = None
-    Cables_bujias_bobinas_ignicion_foto: Optional[str] = None
+    cables_bujias_bobinas_ignicion: Optional[int] = None
+    cables_bujias_bobinas_ignicion_observacion: Optional[str] = None
+    cables_bujias_bobinas_ignicion_foto: Optional[str] = None
 
-    Nivel_anticongenlante: Optional[int] = None
-    Nivel_anticongenlante_observacion: Optional[str] = None
-    Nivel_anticongenlante_foto: Optional[str] = None
+    nivel_anticongenlante: Optional[int] = None
+    nivel_anticongenlante_observacion: Optional[str] = None
+    nivel_anticongenlante_foto: Optional[str] = None
 
-    Tapon_radiador: Optional[int] = None
-    Tapon_radiador_observacion: Optional[str] = None
-    Tapon_radiador_foto: Optional[str] = None
+    tapon_radiador: Optional[int] = None
+    tapon_radiador_observacion: Optional[str] = None
+    tapon_radiador_foto: Optional[str] = None
 
-    Mangueras_sistema: Optional[int] = None
-    Mangueras_sistema_observacion: Optional[str] = None
-    Mangueras_sistema_foto: Optional[str] = None
+    mangueras_sistema: Optional[int] = None
+    mangueras_sistema_observacion: Optional[str] = None
+    mangueras_sistema_foto: Optional[str] = None
 
-    Desempeno_ventilador: Optional[int] = None
-    Desempeno_ventilador_observacion: Optional[str] = None
-    Desempeno_ventilador_foto: Optional[str] = None
+    desempeño_ventilador: Optional[int] = None
+    desempeño_ventilador_observacion: Optional[str] = None
+    desempeño_ventilador_foto: Optional[str] = None
 
-    Calidad_liquido_limpiaparabrisas: Optional[int] = None
-    Calidad_liquido_limpiaparabrisas_observacion: Optional[str] = None
-    Calidad_liquido_limpiaparabrisas_foto: Optional[str] = None
+    calidad_liquido_limpiaparabrisas: Optional[int] = None
+    calidad_liquido_limpiaparabrisas_observacion: Optional[str] = None
+    calidad_liquido_limpiaparabrisas_foto: Optional[str] = None
 
-    Calidad_aceite_direccion_hidraulica: Optional[int] = None
-    Calidad_aceite_direccion_hidraulica_observacion: Optional[str] = None
-    Calidad_aceite_direccion_hidraulica_foto: Optional[str] = None
+    calidad_aceite_direccion_hidraulica: Optional[int] = None
+    calidad_aceite_direccion_hidraulica_observacion: Optional[str] = None
+    calidad_aceite_direccion_hidraulica_foto: Optional[str] = None
 
-    Calidad_aceite_transmision_bayoneta: Optional[int] = None
-    Calidad_aceite_transmision_bayoneta_observacion: Optional[str] = None
-    Calidad_aceite_transmision_bayoneta_foto: Optional[str] = None
+    calidad_aceite_transmision_bayoneta: Optional[int] = None
+    calidad_aceite_transmision_bayoneta_observacion: Optional[str] = None
+    calidad_aceite_transmision_bayoneta_foto: Optional[str] = None
 
-    Liquido_bateria_condiciones: Optional[int] = None
-    Liquido_bateria_condiciones_observacion: Optional[str] = None
-    Liquido_bateria_condiciones_foto: Optional[str] = None
+    liquido_bateria_condiciones: Optional[int] = None
+    liquido_bateria_condiciones_observacion: Optional[str] = None
+    liquido_bateria_condiciones_foto: Optional[str] = None
 
-    Bandas_poly_v: Optional[int] = None
-    Bandas_poly_v_observacion: Optional[str] = None
-    Bandas_poly_v_foto: Optional[str] = None
+    bandas_poly_v: Optional[int] = None
+    bandas_poly_v_observacion: Optional[str] = None
+    bandas_poly_v_foto: Optional[str] = None
 
-    Poleas_banda: Optional[int] = None
-    Poleas_banda_observacion: Optional[str] = None
-    Poleas_banda_foto: Optional[str] = None
+    poleas_banda: Optional[int] = None
+    poleas_banda_observacion: Optional[str] = None
+    poleas_banda_foto: Optional[str] = None
 
-    Banda_tiempo: Optional[int] = None
-    Banda_tiempo_observacion: Optional[str] = None
-    Banda_tiempo_foto: Optional[str] = None
+    banda_tiempo: Optional[int] = None
+    banda_tiempo_observacion: Optional[str] = None
+    banda_tiempo_foto: Optional[str] = None
 
-    Otros_habitaculo_motor: Optional[int] = None
-    Otros_habitaculo_motor_observacion: Optional[str] = None
-    Otros_habitaculo_motor_foto: Optional[str] = None
+    otros_habitaculo_motor: Optional[int] = None
+    otros_habitaculo_motor_observacion: Optional[str] = None
+    otros_habitaculo_motor_foto: Optional[str] = None
 
-    Reset_intervalo_servicio: Optional[int] = None
-    Reset_intervalo_servicio_observacion: Optional[str] = None
-    Reset_intervalo_servicio_foto: Optional[str] = None
+    reset_intervalo_servicio: Optional[int] = None
+    reset_intervalo_servicio_observacion: Optional[str] = None
+    reset_intervalo_servicio_foto: Optional[str] = None
 
-    Ajuste_tornillos_neumaticos_torquimetro: Optional[int] = None
-    Ajuste_tornillos_neumaticos_torquimetro_observacion: Optional[str] = None
-    Ajuste_tornillos_neumaticos_torquimetro_foto: Optional[str] = None
+    ajuste_tornillos_neumaticos_torquimetro: Optional[int] = None
+    ajuste_tornillos_neumaticos_torquimetro_observacion: Optional[str] = None
+    ajuste_tornillos_neumaticos_torquimetro_foto: Optional[str] = None
 
-    Limpiar_libricar_puertas_cerraduras: Optional[int] = None
-    Limpiar_libricar_puertas_cerraduras_observacion: Optional[str] = None
-    Limpiar_libricar_puertas_cerraduras_foto: Optional[str] = None
+    limpiar_libricar_puertas_cerraduras: Optional[int] = None
+    limpiar_libricar_puertas_cerraduras_observacion: Optional[str] = None
+    limpiar_libricar_puertas_cerraduras_foto: Optional[str] = None
 
-    Completar_plan_mantenimiento: Optional[int] = None
-    Completar_plan_mantenimiento_observacion: Optional[str] = None
-    Completar_plan_mantenimiento_foto: Optional[str] = None
+    completar_plan_mantenimiento: Optional[int] = None
+    completar_plan_mantenimiento_observacion: Optional[str] = None
+    completar_plan_mantenimiento_foto: Optional[str] = None
 
     Fecha: str
-    Id_empleado: Optional[int] = None
-    Id_vehiculo: Optional[int] = None
+    IdEmpleado: Optional[int] = None
+    IdVehiculo: Optional[int] = None
     Id_ordendeservicio: Optional[int] = None
     NumeroSerie: str
+
+    #NuevasVariables
